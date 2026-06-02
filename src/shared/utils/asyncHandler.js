3e5d840cea635d59813a4ff48f76924c081a96aa
Roshan Shrestha <stharoshan2005@gmail.com>
2026-06-02 11:34:02 +0545

    add utils apierror apiresponse and asynchandler

---
+++ src/shared/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+import { ApiError } from "./ApiError";
+
+const asyncHandler = (requestHandler) => {
+  return async (req, res, next) => {
+    try {
+      await requestHandler(req, res, next);
+    } catch (error) {
+      console.error(`[asyncHandler] Error caught: `, {
+        message: error.message || error,
+        isApiError: error instanceof ApiError,
+        stack: error.stack || "No stack trace available",
+      });
+
+      if (error instanceof ApiError) {
+        return res.status(error.statusCode || 500).json({
+          success: false,
+          statusCode: error.statusCode,
+          message: error.message,
+          errors: error.errors ? error.errors : [],
+        });
+      }
+
+      return res.status(500).json({
+        success: false,
+        statusCode: 500,
+        message: error.message || "An unexpected error occured",
+      });
+    }
+  };
+};
+
+export { asyncHandler };

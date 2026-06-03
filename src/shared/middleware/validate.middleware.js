65d64bf865e78d1bddabe6e957ecb60a26f9c4b1
Roshan Shrestha <stharoshan2005@gmail.com>
2026-06-03 13:24:21 +0545

    add zod and validate middleware

---
+++ src/shared/middleware/validate.middleware.js
@@ -0,0 +1,23 @@
+export const formatError = (issues) => {
+  const errors = {};
+  for (const issue of issues) {
+    errors[issue.path[0]] = issue.message;
+  }
+
+  return errors;
+};
+
+export const validateBody = (schema) => {
+  return (req, res, next) => {
+    const validation = schema.safeParse(req.body);
+
+    if (!validation.success) {
+      const issues = formatError(validation.error.issues);
+      //   console.log(issues);
+      return next(new ApiError(400, "Invalid registration data", issues));
+    }
+
+    req.body = validation.data;
+    next();
+  };
+};

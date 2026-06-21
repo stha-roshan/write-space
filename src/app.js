fe1211dd839ed7e0e810c11d61d9859259eeb57e
Roshan Shrestha <stharoshan2005@gmail.com>
2026-06-21 15:21:24 +0545

    implemented cookie-parser

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user/user.routes.js";
 import postRoutes from "./routes/posts/post.routes.js";
 // import commentRoutes from "./routes/comments/comment.routes.js";
@@ -9,6 +9,7 @@ import { ApiError } from "./shared/utils/index.js";
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
